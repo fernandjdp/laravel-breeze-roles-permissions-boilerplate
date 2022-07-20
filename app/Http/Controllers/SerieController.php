21840b5147f593262773f6ab6ba53b006eff4605
Fernand Jose Diaz Perez <fdiaz@broota.com>
2022-07-20 15:08:23 -0400

    ADD: Dummy models scaffold

---
+++ app/Http/Controllers/SerieController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSerieRequest;
+use App\Http\Requests\UpdateSerieRequest;
+use App\Models\Serie;
+
+class SerieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSerieRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSerieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Serie  $serie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Serie $serie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Serie  $serie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Serie $serie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSerieRequest  $request
+     * @param  \App\Models\Serie  $serie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSerieRequest $request, Serie $serie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Serie  $serie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Serie $serie)
+    {
+        //
+    }
+}
